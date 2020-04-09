e6e171258a63f721cc2598bff55d30d5645fbb0a
Alejandro Vivanco <manuelinviv@gmail.com>
2020-04-08 20:56:57 -0500

    Cambio a personas y retiro login de peliculas

--- app/Persona.php
+++ app/Persona.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Persona extends Model
 {
-    protected $table='personas';
-    protected $fillable = ['perid','paiid','pernombre','perapellido','perfechanacim','perlugarnacim'];
+    protected $table = 'personas';
+    protected $fillable = ['perid', 'paiid', 'pernombre', 'perapellido', 'perfechanacim', 'perlugarnacim'];
     protected $primaryKey = 'perid';
     public $incrementing = true;
     protected $keyType = 'int';
-    public $timestamps=false;
+    public $timestamps = false;
 }
